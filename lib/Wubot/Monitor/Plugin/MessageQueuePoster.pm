8187ef25c83255c4ba587e7026225be9e6ceba26
Alex White <VVu@geekfarm.org>
2010-11-19 23:56:06 -0800

    add plugin to submit messages from queue to the wubot server

---
+++ lib/Wubot/Monitor/Plugin/MessageQueuePoster.pm
@@ -0,0 +1,51 @@
+package Wubot::Monitor::Plugin::MessageQueuePoster;
+use Moose;
+
+use LWP::UserAgent;
+use HTTP::Request::Common qw{ POST };
+use CGI;
+use YAML;
+
+sub check {
+    my ( $self, $config, $cache ) = @_;
+
+    my @files;
+
+    my $dir_h;
+    opendir( $dir_h, $config->{directory} ) or die "Can't opendir $config->{directory}: $!";
+    while ( defined( my $entry = readdir( $dir_h ) ) ) {
+        next unless $entry;
+        next unless $entry =~ m|\.yaml$|;
+        push @files, $entry;
+    }
+    closedir( $dir_h );
+
+    @files = sort { -M "$config->{directory}/$a" <=> -M "$config->{directory}/$b" } @files;
+
+    return unless scalar @files;
+
+    my $file = pop @files;
+    return unless $file;
+
+    my $path = "$config->{directory}/$file";
+
+    my $data = YAML::LoadFile( $path );
+    $data->{service} = 'wubot-monitor';
+
+    my $data_text = YAML::Dump $data;
+
+    my $ua      = LWP::UserAgent->new();
+    my $request = POST( $config->{url}, [ 'message' => $data_text ] );
+    my $content = $ua->request($request)->as_string();
+
+    #my $cgi = CGI->new();
+    #print $cgi->header(), $content;
+
+    unlink $path;
+
+    return ( undef,
+             $cache,
+         );
+}
+
+1;

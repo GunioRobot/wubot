8f2af40d6d41f8f95a9747cefd997c8075e6ba57
Alex White <VVu@geekfarm.org>
2010-11-24 23:53:55 -0800

    message queue poster - increase delay on each retry
      - store next check in cache
    
      - next retry managed by plugin rather than scheduler
        - deleting cache file causes retry next time check runs
        - downside is that cache file read and written very frequently

--- lib/Wubot/Plugin/MessageQueuePoster.pm
+++ lib/Wubot/Plugin/MessageQueuePoster.pm
@@ -1,6 +1,7 @@
 package Wubot::Plugin::MessageQueuePoster;
 use Moose;
 
+use Growl::Tiny;
 use LWP::UserAgent;
 use HTTP::Request::Common qw{ POST };
 use CGI;
@@ -12,6 +13,11 @@ $hostname =~ s|\..*$||;
 
 sub check {
     my ( $self, $config, $cache ) = @_;
+
+    my $now = time;
+    if ( $cache->{next_retry} && $cache->{next_retry} > $now ) {
+        return ( undef, $cache );
+    }
 
     my @files;
 
@@ -26,32 +32,63 @@ sub check {
 
     @files = sort { -M "$config->{directory}/$a" <=> -M "$config->{directory}/$b" } @files;
 
-    return unless scalar @files;
-
-    my $file = pop @files;
-    return unless $file;
-
-    my $path = "$config->{directory}/$file";
-
-    my $data = YAML::LoadFile( $path );
-    $data->{service} = $data->{key};
-
-    my $data_text = YAML::Dump $data;
-
-    my $ua      = LWP::UserAgent->new();
-    my $request = POST( $config->{url}, [ 'message' => $data_text ] );
-    my $content = $ua->request($request)->as_string();
-
-    if ( $content =~ m|\!OK\!| ) {
-        unlink $path;
-    }
-    else {
-        warn "ERROR: enable to verify message received: $path\n";
+    unless ( scalar @files ) {
+        return ( undef, $cache );
     }
 
-    return ( undef,
-             $cache,
-         );
+    my $max = scalar @files > 10 ? 10 : scalar @files;
+
+  MESSAGE:
+    for my $count ( 1 .. $max ) {
+        my $file = pop @files;
+
+        unless ( $file ) {
+            return ( undef, $cache );
+        }
+
+        my $path = "$config->{directory}/$file";
+
+        my $data = YAML::LoadFile( $path );
+        $data->{service} = $data->{key};
+
+        my $data_text = YAML::Dump $data;
+
+        my $ua      = LWP::UserAgent->new();
+        my $request = POST( $config->{url}, [ 'message' => $data_text ] );
+        my $content = $ua->request($request)->as_string();
+
+        if ( $content =~ m|\!OK\!| ) {
+            unlink $path;
+            $cache->{retry_failures} = 0;
+            $cache->{next_retry} = undef;
+        }
+        else {
+            $cache->{retry_failures}++;
+            $cache->{next_retry} = get_next_retry_utime( $cache->{retry_failures} );
+            my $subject = "$cache->{retry_failures} error(s) sending message, retry after " . scalar localtime( $cache->{next_retry} );
+            Growl::Tiny::notify( { title   => 'Wubot Message Queue',
+                                   subject => $subject,
+                               } );
+            warn "MessageQueuePoster: $subject\n";
+            last MESSAGE;
+        }
+    }
+
+    return ( undef, $cache );
+}
+
+sub get_next_retry_utime {
+    my ( $retry_count ) = @_;
+
+    my $delay = $retry_count * 2 + 5;
+
+    my $now   = time;
+
+    my $next = $delay + $now;
+
+    #$self->logger->debug( "retry_count=$retry_count delay=$delay now=$now retry_next=$next retry_time=" . scalar( localtime( $next ) ) );
+
+    return $next;
 }
 
 1;

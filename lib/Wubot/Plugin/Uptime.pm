208db2270dc667969987edd09787784a05b947f2
Alex White <VVu@geekfarm.org>
2010-11-26 23:44:09 -0800

    prototype plugin to get load averages from 'uptime'
     - whipped up a quick plugin to generate messages for some other testing :P

---
+++ lib/Wubot/Plugin/Uptime.pm
@@ -0,0 +1,25 @@
+package Wubot::Plugin::Uptime;
+use Moose;
+
+with 'Wubot::Plugin::Roles::Cache';
+with 'Wubot::Plugin::Roles::Plugin';
+with 'Wubot::Plugin::Roles::Reactor';
+
+sub check {
+    my ( $self, $config ) = @_;
+
+    my $uptime_output = `uptime`;
+
+    $uptime_output =~ m/load averages?\: ([\d\.]+)\,\s+([\d\.]+),\s+([\d\.]+)/;
+
+    $self->logger->info( "load: $1 => $2 => $3" );
+
+    $self->react( { '1min'  => $1,
+                    '5min'  => $2,
+                    '15min' => $3,
+                } );
+
+    return 1;
+}
+
+1;

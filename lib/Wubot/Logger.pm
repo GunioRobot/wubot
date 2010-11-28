6e171b235d924c8e83417378020c44731d15bfbe
Alex White <VVu@geekfarm.org>
2010-11-28 02:42:13 -0800

    separate library for command line scripts to initialize logging

---
+++ lib/Wubot/Logger.pm
@@ -0,0 +1,45 @@
+package Wubot::Logger;
+use strict;
+use warnings;
+
+use Log::Log4perl qw(:easy);
+
+BEGIN {
+
+    my $log_level = $ENV{LOG_DEBUG} ? 'DEBUG' : 'INFO';
+
+    my $log_name = $0;
+    $log_name =~ s|^.*\/||;
+
+    Log::Log4perl->init(\ <<"EOT");
+        log4perl.category = $log_level, Screen, Logfile
+        log4perl.appender.Screen = Log::Log4perl::Appender::ScreenColoredLevels
+        log4perl.appender.Screen.layout = Log::Log4perl::Layout::PatternLayout
+        log4perl.appender.Screen.layout.ConversionPattern = %d> %m %n
+        log4perl.appender.Screen.color.trace = cyan
+        log4perl.appender.Screen.color.debug = blue
+        log4perl.appender.Screen.color.info  = green
+        log4perl.appender.Screen.color.warn  = magenta
+        log4perl.appender.Screen.color.error = yellow
+        log4perl.appender.Screen.color.fatal = red
+
+        log4perl.appender.Logfile = Log::Dispatch::FileRotate
+        log4perl.appender.Logfile.filename    = $ENV{HOME}/logs/$log_name.log
+        log4perl.appender.Logfile.max         = 10
+        log4perl.appender.Logfile.mode        = append
+        log4perl.appender.Logfile.DatePattern = yyyy-MM-dd
+        log4perl.appender.Logfile.TZ          = PST
+        log4perl.appender.Logfile.layout      = Log::Log4perl::Layout::PatternLayout
+        log4perl.appender.Logfile.layout.ConversionPattern = %d %m %n
+
+EOT
+
+    my $logger = Log::Log4perl::get_logger( __PACKAGE__ );
+
+    $logger->warn( ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>" );
+    $logger->warn( "Logging Initialized..." );
+
+}
+
+
+1;

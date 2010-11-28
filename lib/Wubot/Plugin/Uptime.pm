3a9efc0ddb95cb73833d16441e978f8a3b9520bc
Alex White <VVu@geekfarm.org>
2010-11-27 22:19:57 -0800

    uptime plugin improvements
     - get command from config
     - define config thresholds for warning/critical
     - set status to ok/warning/critical
     - sent 'subject' field in results if any problems seen
     - add validate_config method to validate plugin config on startup

--- lib/Wubot/Plugin/Uptime.pm
+++ lib/Wubot/Plugin/Uptime.pm
@@ -5,18 +5,47 @@ with 'Wubot::Plugin::Roles::Cache';
 with 'Wubot::Plugin::Roles::Plugin';
 with 'Wubot::Plugin::Roles::Reactor';
 
+sub validate_config {
+    my ( $self, $config ) = @_;
+
+    my @required_params = qw( command warning_load critical_load );
+
+    for my $param ( @required_params ) {
+        unless ( $config->{$param} ) {
+            die "ERROR: required config param $param not defined for: ", $self->key, "\n";
+        }
+    }
+
+    return 1;
+}
+
+
 sub check {
     my ( $self, $inputs ) = @_;
 
-    my $uptime_output = `uptime`;
+    my $uptime_output = `$inputs->{config}->{command}`;
 
     $uptime_output =~ m/load averages?\: ([\d\.]+)\,\s+([\d\.]+),\s+([\d\.]+)/;
 
-    $self->logger->info( "load: $1 => $2 => $3" );
+    my ( $load01, $load05, $load15 ) = ( $1, $2, $3 );
 
-    my $results = { '1min'  => $1,
-                    '5min'  => $2,
-                    '15min' => $3,
+    $self->logger->debug( "load: $load01 => $load05 => $load15" );
+
+    my $subject = "";
+    my $status = "ok";
+    if ( $load01 > $inputs->{config}->{critical_load} ) {
+        $subject = "critical: load over last 1 minute is $load01 ";
+        $status = 'critical';
+    } elsif ( $load01 > $inputs->{config}->{warning_load} ) {
+        $subject = "warning: load over last 1 minute is $load01 ";
+        $status = 'warning';
+    }
+
+    my $results = { load01  => $load01,
+                    load05  => $load05,
+                    load15  => $load15,
+                    status  => $status,
+                    subject => $subject,
                 };
 
     return { react => $results };

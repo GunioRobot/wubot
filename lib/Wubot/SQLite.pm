e1d5632614b26d3c60e727d01cf6a3611c4bd070
Alex White <VVu@geekfarm.org>
2010-12-05 14:39:38 -0800

    adding new Wubot::SQLite library for use with reactor plugin
     - automatically creates tables and adds columns on-demand

---
+++ lib/Wubot/SQLite.pm
@@ -0,0 +1,223 @@
+package Wubot::SQLite;
+use Moose;
+
+use DBI;
+use DBD::SQLite;
+
+has 'file' => ( is       => 'ro',
+                isa      => 'Str',
+                required => 1,
+            );
+
+has 'dbh'  => ( is       => 'rw',
+                isa      => 'DBI::db',
+                lazy     => 1,
+                default  => sub {
+                    my ( $self ) = @_;
+                    return $self->connect();
+                },
+            );
+
+sub create_table {
+    my ( $self, $table, $schema_h ) = @_;
+
+    unless ( $table ) {
+        die "Error: table not specified";
+    }
+
+    unless ( $schema_h ) {
+        die( "Error: schema not specified" );
+    }
+
+    my $command = "CREATE TABLE $table (\n";
+
+    my @lines;
+    for my $key ( keys %{ $schema_h } ) {
+        my $type = $schema_h->{$key};
+        push @lines, "\t$key $type";
+    }
+    $command .= join ",\n", @lines;
+
+    $command .= "\n);";
+
+    $self->dbh->do( $command );
+}
+
+sub get_tables {
+    my ( $self, $table ) = @_;
+
+    my $sth = $self->dbh->table_info(undef, undef, $table, 'TABLE' );
+
+    my @tables;
+
+    while ( my $entry = $sth->fetchrow_hashref ) {
+        next if $entry->{TABLE_NAME} eq "sqlite_sequence";
+        push @tables, $entry->{TABLE_NAME};
+    }
+
+    return @tables;
+}
+
+sub insert {
+    my ( $self, $table, $entry, $schema_h ) = @_;
+
+    unless ( $entry && ref $entry eq "HASH" ) {
+        die "ERROR: insert: entry undef or not a hashref";
+    }
+    unless ( $table && $table =~ m|^\w+$| ) {
+        die "ERROR: insert: table name does not look valid"
+    }
+    unless ( $schema_h && ref $schema_h eq "HASH" ) {
+        die "ERROR: no schema specified to insert command!";
+    }
+
+    my ( $command, @keys ) = $self->get_insert_command( $table, $schema_h );
+
+    my $sth1 = $self->get_prepared( $table, $schema_h, $command );
+
+    my @insert = map { $entry->{$_} ? $entry->{$_} : "" } @keys;
+
+    $sth1->execute( @insert );
+
+    return $self->dbh->last_insert_id( "", "", $table, "");
+}
+
+sub query {
+    my ( $self, $statement, $callback ) = @_;
+
+    my $sth = $self->dbh->prepare($statement) or die "Can't prepare $statement\n";
+    my $rv  = $sth->execute or die "can't execute the query: $statement\n";
+
+    my @return;
+
+    while ( my $entry = $sth->fetchrow_hashref ) {
+        if ( $callback ) {
+            $callback->( $entry );
+        }
+        else {
+            push @return, $entry;
+        }
+    }
+
+    return @return;
+}
+
+sub delete {
+    my ( $self, $table, $conditions ) = @_;
+
+    unless ( $table && $table =~ m|^\w+$| ) {
+        die "ERROR: delete: invalid table name";
+    }
+    unless ( $conditions && ref $conditions eq "HASH" ) {
+        die "ERROR: delete: conditions is not a hash ref"
+    }
+
+    my $delete = "DELETE FROM $table WHERE ";
+
+    my @conditions;
+    for my $key ( keys %{ $conditions } ) {
+        push @conditions, "$key = '$conditions->{$key}'";
+    }
+
+    $delete .= join( " AND ", @conditions );
+
+    $self->dbh->do( $delete );
+}
+
+
+sub get_insert_command {
+    my ( $self, $table, $schema_h ) = @_;
+
+    # temporary schema hash, remove the id
+    my $insert_schema_h = { %{ $schema_h } };
+    delete $insert_schema_h->{id};
+
+    # create the insert statement
+    my @keys = sort keys %{ $insert_schema_h };
+    my $insert = join( ",", @keys );
+
+    my $values = "?," x $#keys . "?";
+    my $command = qq{ INSERT INTO $table ( $insert )
+                       VALUES ($values)
+     };
+
+    return $command, @keys;
+}
+
+
+sub get_prepared {
+    my ( $self, $table, $schema, $command ) = @_;
+
+    my $sth1;
+
+  RETRY:
+    for my $retry ( 0 .. 10 ) {
+        eval {                          # try
+            $sth1 = $self->dbh->prepare( $command );
+            1;
+        } or do {                       # catch
+            my $error = $@;
+
+            # if the table doesn't already exist, create it
+            if ( $error =~ m/no such table/ ) {
+                print "Creating missing table: $table\n";
+                $self->create_table( $table, $schema );
+                $self->{tables}->{$table} = 1;
+                next RETRY;
+            } elsif ( $error =~ m/(?:has no column named|no such column\:) (\S+)/ ) {
+                my $column = $1;
+
+                unless ( $column ) { die "ERROR: failed to capture a column name!" }
+
+                print "Adding missing column: $column\n";
+
+                if ( $schema->{$column} ) {
+                    $self->add_column( $table, $column, $schema->{$column} );
+                    next RETRY;
+                } else {
+                    die "Missing column not defined in schema: $column";
+                }
+            } else {
+                die "Unhandled error: $error";
+            }
+
+        };
+
+        return $sth1;
+    };
+
+    die "ERROR: unable to prepare statement, exceeded maximum retry limit";
+}
+
+sub add_column {
+    my ( $self, $table, $column, $type ) = @_;
+    my $command = "ALTER TABLE $table ADD COLUMN $column $type";
+    $self->dbh->do( $command );
+}
+
+
+sub connect {
+    my ( $self ) = @_;
+
+    my $datafile = $self->file;
+    print "Opening sqlite file: $datafile\n";
+
+    my $dbh = DBI->connect(
+        "dbi:SQLite:$datafile", "", "",
+        {
+            AutoCommit => 1,
+            RaiseError => 1,
+        }
+    ) or die "Unable to create database handle: $!";
+
+    return $dbh;
+}
+
+sub disconnect {
+    my ( $self ) = @_;
+
+    $self->dbh->disconnect;
+}
+
+
+1;
---
+++ t/wubot_sqlite.t
@@ -0,0 +1,103 @@
+#!/perl
+use strict;
+
+use File::Temp qw/ tempdir /;
+use Test::Exception;
+use Test::More 'no_plan';
+use YAML;
+
+use Wubot::SQLite;
+
+
+my $tempdir = tempdir( "/tmp/tmpdir-XXXXXXXXXX", CLEANUP => 1 );
+
+my $sqldb = "$tempdir/test.sql";
+
+ok( my $sql = Wubot::SQLite->new( { file => $sqldb } ),
+    "Creating new Wubot::SQLite object"
+);
+
+ok( $sql->dbh,
+    "Forcing dbh connection to lazy load"
+);
+
+ok( -r $sqldb,
+    "Checking that sql db was created: $sqldb"
+);
+
+{
+    my $table = "test_table_1";
+    my $schema = { column1 => 'INT',
+                   column2 => 'VARCHAR(16)',
+               };
+
+    ok( $sql->create_table( $table, $schema ),
+        "Creating a table $table"
+    );
+
+    is_deeply( [ $sql->get_tables() ],
+               [ $table ],
+               "Checking that table was created"
+           );
+
+    ok( $sql->insert( $table, { column1 => 123, column2 => "foo", column3 => "abc" }, $schema ),
+        "Inserting hash into table"
+    );
+
+    is( ( $sql->query( "SELECT * FROM $table" ) )[0]->{column2},
+        'foo',
+        "Selecting row just inserted into table and checking column value"
+    );
+
+    is( ( $sql->query( "SELECT * FROM $table" ) )[0]->{column3},
+        undef,
+        "Checking that key not defined in schema was not inserted into table"
+    );
+
+    ok( $sql->delete( $table, { column1 => 123 } ),
+        "Deleting entry just added"
+    );
+
+    is_deeply( [ $sql->query( "SELECT * FROM $table" ) ],
+               [],
+               "Checking that no rows left in the table"
+           );
+
+    $schema->{column3} = 'VARCHAR(32)';
+
+    ok( $sql->insert( $table, { column1 => 234, column2 => "bar", column3 => "baz" }, $schema ),
+        "Inserting hash with modified schema to include column3"
+    );
+
+    is( ( $sql->query( "SELECT * FROM $table" ) )[0]->{column3},
+        'baz',
+        "Selecting column3 just inserted into table and checking column value"
+    );
+
+}
+
+{
+    my $table = "test_table_2";
+    my $schema = { column1 => 'INT',
+                   column2 => 'VARCHAR(16)',
+               };
+
+    ok( $sql->insert( $table, { column1 => 123, column2 => "foo", column3 => "abc" }, $schema ),
+        "Inserting hash into non-existent table"
+    );
+
+    is( ( $sql->query( "SELECT * FROM $table" ) )[0]->{column2},
+        'foo',
+        "Checking that table was created and data was inserted and retrieved"
+    );
+
+}
+
+ok( $sql->disconnect(),
+    "Closing SQLite file"
+);
+
+throws_ok( sub { $sql->query( "SELECT * FROM test_table_2" ) },
+           qr/prepare failed.*inactive database handle/,
+           "Checking that exception thrown when running a sql query on dead sql handle",
+       );

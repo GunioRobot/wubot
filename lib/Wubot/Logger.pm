54cd9798c16f3913a9d821e4cbdcb8a7cf335c97
Alex White <VVu@geekfarm.org>
2010-12-20 17:36:19 -0800

    logger - always write debug output to log file

--- lib/Wubot/Logger.pm
+++ lib/Wubot/Logger.pm
@@ -12,10 +12,11 @@ BEGIN {
     $log_name =~ s|^.*\/||;
 
     Log::Log4perl->init(\ <<"EOT");
-        log4perl.category = $log_level, Screen, Logfile
+        log4perl.category = DEBUG, Screen, Logfile
         log4perl.appender.Screen = Log::Log4perl::Appender::ScreenColoredLevels
         log4perl.appender.Screen.layout = Log::Log4perl::Layout::PatternLayout
         log4perl.appender.Screen.layout.ConversionPattern = %d> %m %n
+        log4perl.appender.Screen.Threshold   = $log_level
         log4perl.appender.Screen.color.trace = cyan
         log4perl.appender.Screen.color.debug = blue
         log4perl.appender.Screen.color.info  = green

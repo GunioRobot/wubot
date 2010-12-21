2c9f1b7ac167ee59fde12c38234e38a2d4929a50
Alex White <VVu@geekfarm.org>
2010-12-21 15:31:35 -0800

    adding a plugin to count occurrences of regexps in growing log files
     - uses Wubot::Tail to process new lines, tail -f style

---
+++ lib/Wubot/Plugin/FileRegexp.pm
@@ -0,0 +1,72 @@
+package Wubot::Plugin::FileRegexp;
+use Moose;
+
+use Log::Log4perl;
+
+use Wubot::Tail;
+
+has 'path'      => ( is      => 'rw',
+                     isa     => 'Str',
+                     default => '',
+                 );
+
+has 'tail'      => ( is      => 'ro',
+                     isa     => 'Wubot::Tail',
+                     lazy    => 1,
+                     default => sub {
+                         my ( $self ) = @_;
+                         return Wubot::Tail->new( { path  => $self->path } );
+                     },
+                 );
+
+has 'logger'    => ( is      => 'ro',
+                     isa     => 'Log::Log4perl::Logger',
+                     lazy    => 1,
+                     default => sub {
+                         return Log::Log4perl::get_logger( __PACKAGE__ );
+                     },
+                 );
+
+with 'Wubot::Plugin::Roles::Cache';
+with 'Wubot::Plugin::Roles::Plugin';
+
+sub init {
+    my ( $self, $inputs ) = @_;
+
+    $self->{react} = $inputs->{cache};
+    delete $self->{react}->{lastupdate};
+
+    $self->path( $inputs->{config}->{path} );
+
+    my $callback = sub {
+        my $line = shift;
+        for my $regexp_name ( keys %{ $inputs->{config}->{regexp} } ) {
+
+            my $regexp = $inputs->{config}->{regexp}->{ $regexp_name };
+
+            if ( $line =~ m|$regexp| ) {
+                $self->{react}->{ $regexp_name }++;
+            }
+        }
+    };
+
+    $self->tail->callback( $callback );
+
+    $self->tail->reset_callback( sub { print YAML::Dump @_ } );
+
+    return;
+}
+
+sub check {
+    my ( $self, $inputs ) = @_;
+
+    $self->tail->get_lines();
+
+    if ( $self->{react} ) {
+        return { react => { %{ $self->{react} } }, cache => { %{ $self->{react} } } };
+    }
+
+    return;
+}
+
+1;
---
+++ t/file_regexp.t
@@ -0,0 +1,59 @@
+#!/perl
+use strict;
+
+use File::Temp qw/ tempdir /;
+use Log::Log4perl qw(:easy);
+use Test::More 'no_plan';
+use YAML;
+
+Log::Log4perl->easy_init($INFO);
+
+use Wubot::Plugin::FileRegexp;
+
+my $init = { key        => 'FileRegexp-testcase',
+             class      => 'Wubot::Plugin::FileRegexp',
+             cache_file => '/dev/null',
+         };
+
+my $tempdir = tempdir( "/tmp/tmpdir-XXXXXXXXXX", CLEANUP => 1 );
+
+$| = 1;
+
+{
+    my $path = "$tempdir/file1.log";
+
+    ok( my $tail = Wubot::Plugin::FileRegexp->new( $init ),
+        "Creating new file tail object"
+    );
+
+    ok( ! $tail->init( { config => { path   => $path,
+                                     regexp => { testall => 'test',
+                                                 testone => 'test1',
+                                             },
+                                 } } ),
+        "Initializing tail plugin"
+    );
+
+    system( "touch $path" );
+
+    ok( $tail->check(),
+        "No lines on initial check"
+    );
+
+    system( "echo test1 match >> $path" );
+    system( "echo test2 match >> $path" );
+
+    ok( my $results = $tail->check(),
+        "Calling check() now that a couple of lines were added"
+    );
+
+    is( $results->{react}->{testall},
+        2,
+        "Checking that two lines matched 'test' regexp"
+    );
+
+    is( $results->{react}->{testone},
+        1,
+        "Checking that one lines matched 'test1' regexp"
+    );
+}

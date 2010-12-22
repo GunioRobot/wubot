7483850b8ebf5298906c102e2c5f04611ce95090
Alex White <VVu@geekfarm.org>
2010-12-22 01:24:47 -0800

    add reactor plugin to delete matching sqlite rows

---
+++ lib/Wubot/Reactor/SQLiteDelete.pm
@@ -0,0 +1,39 @@
+package Wubot::Reactor::SQLiteDelete;
+use Moose;
+
+use Log::Log4perl;
+use YAML;
+
+use Wubot::SQLite;
+
+has 'logger'  => ( is => 'ro',
+                   isa => 'Log::Log4perl::Logger',
+                   lazy => 1,
+                   default => sub {
+                       return Log::Log4perl::get_logger( __PACKAGE__ );
+                   },
+               );
+
+has 'sqlite'  => ( is => 'ro',
+                   isa => 'HashRef',
+                   default => sub { {} },
+               );
+
+sub react {
+    my ( $self, $message, $config ) = @_;
+
+    my $sqlite;
+
+    # if we don't have a sqlite object for this file, create one now
+    unless ( $self->sqlite->{ $config->{file} } ) {
+        $self->sqlite->{ $config->{file} } = Wubot::SQLite->new( { file => $config->{file} } );
+    }
+
+    my $field = $config->{where_field};
+
+    $self->sqlite->{ $config->{file} }->delete( $config->{tablename}, { $field => $message->{$field} } );
+
+    return $message;
+}
+
+1;

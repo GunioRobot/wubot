a64eb8fef4c9a9a41cce6e077e50edfca53f8ddf
Alex White <VVu@geekfarm.org>
2011-01-02 18:46:07 -0800

    GRID::Machine powered remote queue fetcher - minor refinements
     - still need to work on reconnecting after failure

--- lib/Wubot/Plugin/RemoteQueue.pm
+++ lib/Wubot/Plugin/RemoteQueue.pm
@@ -22,6 +22,16 @@ has 'reactor'  => ( is => 'ro',
                     required => 1,
                 );
 
+has 'count'    => ( is => 'rw',
+                    isa => 'Num',
+                    default => 0,
+                );
+
+has 'lastfetch' => ( is => 'rw',
+                     isa => 'Num',
+                     default => 0,
+                 );
+
 with 'Wubot::Plugin::Roles::Cache';
 with 'Wubot::Plugin::Roles::Plugin';
 
@@ -29,25 +39,48 @@ sub check {
     my ( $self, $inputs ) = @_;
 
     my $config = $inputs->{config};
+    my $cache  = $inputs->{cache};
+
+    $self->logger->debug( "Checking remote queue on $config->{host}" );
 
     unless ( $self->machine ) {
+        $self->logger->warn( "CONNECTING TO HOST: $config->{host}" );
         $self->machine( $self->create_machine( $config ) );
         $self->reactor->( { subject => "GRID::Machine connecting to $config->{host}" } );
         return;
     }
 
+    my $count = 0;
+
   MESSAGE:
-    for my $message ( 1 .. 10 ) {
+    for my $message ( 1 .. 100 ) {
         my $results = $self->machine->get_message( $config->{path} );
 
-        next MESSAGE unless $results->{results}->[0];
+        if ( $results->{errmsg} ) {
+            $self->reactor->( { subject => "ERROR: $results->{errmsg}" } );
+            last MESSAGE;
+        }
+
+        last MESSAGE unless $results->{results}->[0];
 
         $self->reactor->( $results->{results}->[0] );
 
         $self->machine->processed_message();
+
+        $cache->{lastfetch} = time;
+
+        $count++;
     }
 
-    return;
+    if ( $count && $count > 10 ) {
+        $self->logger->info( $self->key, ": fetched $count messages" );
+    }
+
+    if ( $cache->{lastfetch} + 300 < time ) {
+        $self->reactor->( { subject => "ERROR: no messages fetched in more than 5 minutes" } );
+    }
+
+    return { cache => $cache };
 }
 
 sub create_machine {
@@ -64,11 +97,11 @@ sub create_machine {
         my $results = $machine->sub(
             init        => q{
                 use Log::Log4perl qw(:easy);
-                Log::Log4perl->easy_init($INFO);
-                use Wubot::LocalMessageStore;
-                $main::messenger = Wubot::LocalMessageStore->new();
+                Log::Log4perl->easy_init($DEBUG);
+                return 1;
              }
         );
+        YAML::Dump $results;
         die $results->errmsg unless $results->ok;
     }
 
@@ -76,11 +109,19 @@ sub create_machine {
         my $results = $machine->sub(
             get_message => q{
                 my $path = $_[0];
+
+                unless ( $main::messenger ) {
+                    use Wubot::LocalMessageStore;
+                    $main::messenger = Wubot::LocalMessageStore->new();
+                }
+
                 my ( $message, $callback ) = $main::messenger->get( $path );
+
                 $main::callback = $callback;
                 return $message;
             },
         );
+        YAML::Dump $results;
         die $results->errmsg unless $results->ok;
     }
 
@@ -90,10 +131,11 @@ sub create_machine {
                 $main::callback->();
             },
         );
+        YAML::Dump $results;
         die $results->errmsg unless $results->ok;
     }
 
-    my $init_results = $machine->init();
+    $machine->init();
 
     return $machine;
 }

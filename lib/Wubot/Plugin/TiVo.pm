c116855b5c058efe43c840071543311de2e81b73
Alex White <VVu@geekfarm.org>
2011-01-12 12:35:03 -0800

    tivo plugin - wrap check in an eval block
     - temporary fix until Check() can fork checks as child processes

--- lib/Wubot/Plugin/TiVo.pm
+++ lib/Wubot/Plugin/TiVo.pm
@@ -34,56 +34,64 @@ sub check {
         }
     }
 
-    my $tivo = Net::TiVo->new(
-        host  => $config->{host},
-        mac   => $config->{key},
-    );
+    eval {                          # try
 
-    for my $folder ($tivo->folders()) {
+        my $tivo = Net::TiVo->new(
+            host  => $config->{host},
+            mac   => $config->{key},
+        );
 
-        my $folder_string = $folder->as_string();
-        print "TIVO FOLDER: $folder_string\n";
+        for my $folder ($tivo->folders()) {
 
-        next if $folder_string =~ m|^HD Recordings|;
+            my $folder_string = $folder->as_string();
+            print "TIVO FOLDER: $folder_string\n";
 
-      SHOW:
-        for my $show ($folder->shows()) {
-            my $show_string = $show->as_string();
+            next if $folder_string =~ m|^HD Recordings|;
 
-            next SHOW if $cache->{shows}->{$show_string};
-            next SHOW if $show->in_progress();
+          SHOW:
+            for my $show ($folder->shows()) {
+                my $show_string = $show->as_string();
 
-            my $subject = join ": ", $show->name(), $show->episode();
+                next SHOW if $cache->{shows}->{$show_string};
+                next SHOW if $show->in_progress();
 
-            # duration in minutes
-            my $duration = int( $show->duration() / 60000 );
+                my $subject = join ": ", $show->name(), $show->episode();
 
-            # size in MB
-            my $size     = int( $show->size() / 1000000 );
+                # duration in minutes
+                my $duration = int( $show->duration() / 60000 );
 
-            $self->reactor->( { subject     => $subject,
-                                name        => $show->name(),
-                                episode     => $show->episode(),
-                                episode_num => $show->episode_num(),
-                                recorded    => $show->capture_date(),
-                                format      => $show->format(),
-                                hd          => $show->high_definition(),
-                                size        => $size,
-                                channel     => $show->channel(),
-                                duration    => $duration,
-                                description => $show->description(),
-                                program_id  => $show->program_id(),
-                                series_id   => $show->series_id(),
-                                link        => $show->url(),
-                            } );
+                # size in MB
+                my $size     = int( $show->size() / 1000000 );
 
-            $cache->{shows}->{$show_string} = 1;
+                $self->reactor->( { subject     => $subject,
+                                    name        => $show->name(),
+                                    episode     => $show->episode(),
+                                    episode_num => $show->episode_num(),
+                                    recorded    => $show->capture_date(),
+                                    format      => $show->format(),
+                                    hd          => $show->high_definition(),
+                                    size        => $size,
+                                    channel     => $show->channel(),
+                                    duration    => $duration,
+                                    description => $show->description(),
+                                    program_id  => $show->program_id(),
+                                    series_id   => $show->series_id(),
+                                    link        => $show->url(),
+                                } );
 
+                $cache->{shows}->{$show_string} = 1;
+
+            }
         }
-    }
 
-    # write out the updated cache
-    $self->write_cache( $cache );
+        # write out the updated cache
+        $self->write_cache( $cache );
+
+        1;
+    } or do {                   # catch
+
+        $self->logger->info( "ERROR: getting tivo info: $@" );
+    };
 
     exit 0;
 }

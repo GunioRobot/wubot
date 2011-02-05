6f1a3f87cfff2eaf1ac04d694b91a481fe5dd0df
Alex White <VVu@geekfarm.org>
2011-02-04 23:29:47 -0800

    add a plugin to monitor OS X volume
     - send a notification when
       - muted
       - below configured threshold

---
+++ lib/Wubot/Plugin/OsxVolume.pm
@@ -0,0 +1,33 @@
+package Wubot::Plugin::OsxVolume;
+use Moose;
+
+with 'Wubot::Plugin::Roles::Cache';
+with 'Wubot::Plugin::Roles::Plugin';
+
+sub check {
+    my ( $self, $inputs ) = @_;
+
+    my $cache  = $inputs->{cache};
+    my $config = $inputs->{config};
+
+    my $settings;
+
+    for my $setting ( split /\, /, `osascript -e 'get volume settings'` ) {
+
+        $setting =~ m|^(.*)\:(.*)$|;
+
+        $settings->{ $1 } = $2;
+    }
+
+    if ( $config->{alert_muted} && $settings->{'output muted'} eq "true" ) {
+        return { react => { subject => 'volume muted' } };
+    }
+
+    if ( $settings->{'output volume'} < $config->{min_volume} ) {
+        return { react => { subject => "low volume: $settings->{'output volume'}" } };
+    }
+
+    return;
+}
+
+1;

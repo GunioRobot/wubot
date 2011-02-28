140191df8502b683a8b01526c437a69de7440bd1
Alex White <VVu@geekfarm.org>
2011-02-27 21:03:49 -0800

    plugin to check the last time a path was updated
     - generate an alert if it hasn't been updated in specified amount of time
    
     - first use--monitoring when itunes has stopped downloading my podcasts

---
+++ lib/Wubot/Plugin/PathLastUpdate.pm
@@ -0,0 +1,34 @@
+package Wubot::Plugin::PathLastUpdate;
+use Moose;
+
+with 'Wubot::Plugin::Roles::Cache';
+with 'Wubot::Plugin::Roles::Plugin';
+
+use Wubot::TimeLength;
+my $timelength = Wubot::TimeLength->new();
+
+sub check {
+    my ( $self, $inputs ) = @_;
+
+    my $cache  = $inputs->{cache};
+    my $config = $inputs->{config};
+
+    my $path = $config->{path};
+
+    my $last_modified = ( stat $path )[9];
+
+    my $age = time - $last_modified;
+
+    my $seconds = $timelength->get_seconds( $config->{age} );
+
+    if ( $age > $seconds ) {
+
+        my $time_passed = $timelength->get_human_readable( $age );
+
+        return { react => { subject => "path has not been updated in $time_passed!" } };
+    }
+
+    return;
+}
+
+1;

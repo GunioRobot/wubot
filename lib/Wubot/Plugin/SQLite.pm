f4940761edfcb47da7884465270625cc12c6ab81
Alex White <VVu@geekfarm.org>
2011-06-23 18:26:49 -0700

    added prototype SQLite plugin

---
+++ lib/Wubot/Plugin/SQLite.pm
@@ -0,0 +1,50 @@
+package Wubot::Plugin::SQLite;
+use Moose;
+
+# VERSION
+
+with 'Wubot::Plugin::Roles::Cache';
+with 'Wubot::Plugin::Roles::Plugin';
+
+sub check {
+    my ( $self, $inputs ) = @_;
+
+    my $cache  = $inputs->{cache};
+    my $config = $inputs->{config};
+
+    my @react;
+
+    my $file = glob( $config->{dbfile} );
+
+    my $sqlite =  Wubot::SQLite->new( { file => $file } );
+
+    if ( $config->{statements} ) {
+        my $return;
+
+        for my $statement ( @{ $config->{statements} } ) {
+            for my $row ( $sqlite->query( $statement ) ) {
+
+                for my $key ( keys %{ $row } ) {
+                    $return->{$key} = $row->{$key};
+                }
+            }
+        }
+
+        push @react, $return;
+    }
+    elsif ( $config->{statement} ) {
+        for my $row ( $sqlite->query( $config->{statement} ) ) {
+
+            if ( $row->{id} ) {
+                next if $self->cache_is_seen( $cache, $row->{id} );
+                $self->cache_mark_seen( $cache, $row->{id} );
+            }
+
+            push @react, $row;
+        }
+    }
+
+    return { cache => $cache, react => \@react };
+}
+
+1;

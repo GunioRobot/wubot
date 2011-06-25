de49bda8be7c68dc5b8bca82049203040e9dfc99
Alex White <VVu@geekfarm.org>
2011-06-24 20:40:48 -0700

    'split' reactor module - prevent uninitialized value warnings

--- lib/Wubot/Reactor/Split.pm
+++ lib/Wubot/Reactor/Split.pm
@@ -9,6 +9,8 @@ use YAML;
 sub react {
     my ( $self, $message, $config ) = @_;
 
+    return $message unless $config->{source_field};
+
     my @data = split /\s*,\s*/, $message->{ $config->{source_field} };
 
     for my $field ( reverse @{ $config->{target_fields} } ) {

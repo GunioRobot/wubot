9d9c3b574b0d5203a7ad218cd3b25a600862ca95
Alex White <VVu@geekfarm.org>
2011-06-24 19:47:09 -0700

    added 'split' reactor plugin
      - split one csv field into other fields

---
+++ lib/Wubot/Reactor/Split.pm
@@ -0,0 +1,21 @@
+package Wubot::Reactor::Split;
+use Moose;
+
+# VERSION
+
+use HTML::Strip;
+use YAML;
+
+sub react {
+    my ( $self, $message, $config ) = @_;
+
+    my @data = split /\s*,\s*/, $message->{ $config->{source_field} };
+
+    for my $field ( reverse @{ $config->{target_fields} } ) {
+        $message->{ $field } = pop @data;
+    }
+
+    return $message;
+}
+
+1;

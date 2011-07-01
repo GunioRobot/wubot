6206ab2a56d2fb816a91931ac8840db4106c9949
Alex White <VVu@geekfarm.org>
2011-06-30 22:31:52 -0700

    update Outlook plugin to only report inbox count
      - getting message body causes message to be marked as seen in outlook
    
      - have some additional improvements planned here

--- lib/Wubot/Plugin/Outlook.pm
+++ lib/Wubot/Plugin/Outlook.pm
@@ -36,40 +36,13 @@ sub check {
         return;
     }
 
-    my $count = 0;
+    my $count = scalar $self->get_msgids( $content );
 
-  MSGID:
-    for my $msgid ( $self->get_msgids( $content ) ) {
-
-        # if this id is already in the db, we're done
-        next MSGID if $self->cache_is_seen( $cache, $msgid );
-
-        # only get 10 unread messages per iteration
-        $count++;
-        last MSGID if $count > 10;
-
-        my $msg_url = join '?', $config->{url}, "ae=Item&t=IPM.Note&id=$msgid";
-        my $content = $self->get_content( $msg_url, $config );
-
-        next MSGID unless $content;
-
-        my $entry = $self->parse_message( $content, $msg_url );
-        $entry->{link} = $msg_url;
-
-        push @react, $entry;
-
-        $self->cache_mark_seen( $cache, $msgid );
+    if ( $count > 1 ) {
+        push @react, { subject => "$count messages in your inbox" };
     }
 
-    $self->cache_expire( $cache );
-
-    my $results = { cache => $cache, react => \@react };
-
-    if ( $count > 10 ) {
-        $results->{delay} = 10;
-    }
-
-    return $results;
+    return { react => \@react };
 }
 
 sub get_msgids {
@@ -87,146 +60,7 @@ sub get_msgids {
         push @return, $msgid;
     }
 
-    return reverse @return;
-}
-
-sub parse_message {
-    my ( $self, $content, $url ) = @_;
-
-    my $te = HTML::TableExtract->new( keep_html => 1 );
-
-    # "Parsing of undecoded UTF-8 will give garbage when decoding entities"
-    $content = Encode::decode_utf8( $content );
-
-    $te->parse( $content );
-
-    my @headers;
-
-    # extract the headers
-    $content =~ m|(<table class="msgHd".*?</table>)|s;
-    my $header = $1;
-
-    unless ( $header ) {
-        die "ERROR: no message retrieved!"
-    }
-
-    my $header_data;
-
-    for my $row ( split /\<tr\s*\>/, $header ) {
-        $row =~ s|^\s+||mg;
-        #print "ROW: $row\n";
-
-        if ( $row =~ m|<td[^>]+class="(\w+)">| ) {
-
-            my $class = $1;
-            #print "CLASS: $class\n";
-
-            if    ( $class eq "sub" ) { $header_data->{subject}  = $row }
-            elsif ( $class eq "frm" ) { $header_data->{username} = $row;
-                                    }
-            elsif ( $class eq "hdtxt" ) {
-
-                my $field_name;
-
-                for my $subrow ( split /\<td/, $row ) {
-
-                    next unless $subrow;
-
-                    if ( $subrow =~ m|class="hdtxt">\s*(.*)\:\s*</td>|s ) {
-                        $field_name = $1;
-                        #print "FIELD NAME: $field_name\n";
-                    }
-                    elsif ( $subrow =~ m|class="hdtxnr">\s*(.*)\s*</td>|s ) {
-                        my $value = $1;
-                        #print "VALUE: $value\n";
-                        $header_data->{$field_name} = $value;
-                    }
-                    else {
-                        print "UNPARSED: $subrow\n";
-                    }
-                }
-
-                #print "\n";
-
-
-            }
-            else {
-                #print "UNHANDLED Class: $1\n$row\n\n";
-            }
-        }
-        else {
-            #print "NO CLASS:\n$row\n";
-        }
-
-    }
-
-    for my $field ( keys %{ $header_data } ) {
-
-        $header_data->{$field} =~ s|^\s+||;
-        $header_data->{$field} =~ s|\s+$||;
-        $header_data->{$field} =~ s/\<\/?(span|div).*?\>//g;
-        $header_data->{$field} =~ s|</a>||g;
-        $header_data->{$field} =~ s|<a href.*?>||g;
-
-    }
-
-    unless ( $header ) {
-        $self->logger->info( "critical: outlook: Unable to parse outlook page content" );
-    }
-
-    my $subject = $header_data->{subject};
-    $subject =~ s|^<td.*?>||s;
-    $subject =~ s|</td.*$||s;
-    $subject =~ s|^[\r\n]+||s;
-    $subject =~ s|[\r\n]+$||s;
-    #print "OUTLOOK SUBJECT: $subject\n";
-
-    my $username = $header_data->{username};
-    $username =~ s|^<td.*?>||s;
-    $username =~ s|</td.*$||s;
-    $username =~ s|^[\r\n]+||s;
-    $username =~ s|[\r\n]+$||s;
-    #print "USERNAME: $username\n";
-
-    my $sent = $header_data->{Sent};
-    my $sent_utime = UnixDate( ParseDate( $sent ), "%s" );
-    #print "SENT: $sent [$sent_utime]\n";
-
-    my $to_user = $header_data->{To};
-    #print "TO: $to_user\n";
-
-    # extract the body
-    my $body = $content;
-    $body =~ s|^.*\<td class\=\"bdy\"\>||s;
-    $body =~ s|<td class\=\"nvft\"\>.*$||s;
-    $body =~ s|^(.*)<\/div>.*$|$1|sg;
-    $body =~ s|^\s+<div |<div |mg;
-    $body = Encode::encode_utf8( $body );
-    $body =~ s|\r||g;
-    $body =~ s|\s*$||mg;
-    #print "BODY: $body\n";
-    #$body =~ s|img[^\<\>]+src="attachment.ashx|img src="$url/attachment.ashx|sg;
-
-    # fix chars
-    $body =~ s|\xe2\x80\x93|-|g;
-    $body =~ s|\xe2\x80\x98|'|g;
-    $body =~ s|\xe2\x80\x99|'|g;
-    $body =~ s|\xe2\x80\xa6|...|g;
-
-    utf8::decode( $body );
-
-    my $entry = { subject    => $subject,
-                  username   => $username,
-                  sent       => $sent,
-                  lastupdate => $sent_utime,
-                  to_user    => $to_user,
-                  body       => $body,
-              };
-
-    #$entry->{body} = Encode::encode_utf8( $entry->{body} );
-    #print YAML::Dump $entry->{body};
-
-    return $entry;
+    return @return;
 }
 
 sub get_content {

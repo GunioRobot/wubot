a64dc6dc03687bb96b7794d8f28621ddb88967fa
Alex White <VVu@geekfarm.org>
2011-07-13 01:56:35 -0700

    add warning if SetField reaction called without a field

--- lib/Wubot/Reactor/SetField.pm
+++ lib/Wubot/Reactor/SetField.pm
@@ -3,11 +3,26 @@ use Moose;
 
 # VERSION
 
+use Log::Log4perl;
+
+has 'logger'  => ( is => 'ro',
+                   isa => 'Log::Log4perl::Logger',
+                   lazy => 1,
+                   default => sub {
+                       return Log::Log4perl::get_logger( __PACKAGE__ );
+                   },
+               );
+
 sub react {
     my ( $self, $message, $config ) = @_;
 
     if ( $config->{no_override} ) {
         return $message if $message->{ $config->{field} };
+    }
+
+    unless ( $config->{field} ) {
+        $self->logger->warn( "ERROR: No field in SetField config: ", YAML::Dump $config );
+        return $message;
     }
 
     $message->{ $config->{field} } = $config->{value};

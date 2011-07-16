c33eb38ea617d192e25a6817e5dc023e2a0953a7
Alex White <VVu@geekfarm.org>
2011-07-16 00:39:30 -0700

    add background commands and queueing to Wubot::Reactor::Command
      - fork a child process to run a command in background
    
      - create a lock file with the child pid, prevent forking multiple child processes
    
      - child process writes its stdout/stderr to a log file
    
      - commands are queued if lock file exists
    
      - reactor's monitor() method
        - finds finished processes and to send a completion method
        - starts the next command in the queue
    
      - still a number of loose ends left to tie up

--- lib/Wubot/Reactor/Command.pm
+++ lib/Wubot/Reactor/Command.pm
@@ -3,30 +3,80 @@ use Moose;
 
 # VERSION
 
+use File::Path;
 use Log::Log4perl;
-use POSIX qw(strftime);
+use POSIX qw(strftime setsid :sys_wait_h);
 use Term::ANSIColor;
 
-has 'logger'  => ( is       => 'ro',
-                   isa      => 'Log::Log4perl::Logger',
-                   lazy     => 1,
-                   default  => sub {
-                       return Log::Log4perl::get_logger( __PACKAGE__ );
-                   },
-               );
+use Wubot::LocalMessageStore;
+
+has 'logger'   => ( is       => 'ro',
+                    isa      => 'Log::Log4perl::Logger',
+                    lazy     => 1,
+                    default  => sub {
+                        return Log::Log4perl::get_logger( __PACKAGE__ );
+                    },
+                );
+
+has 'logdir'   => ( is       => 'ro',
+                    isa      => 'Str',
+                    lazy     => 1,
+                    default  => sub {
+                        my $self = shift;
+                        return join( "/", $ENV{HOME}, "wubot", "commands" );
+                    },
+                );
+
+has 'queue'   => ( is      => 'ro',
+                     isa     => 'Wubot::LocalMessageStore',
+                     lazy    => 1,
+                     default => sub {
+                         return Wubot::LocalMessageStore->new();
+                     },
+                 );
+
+has 'queuedir'  => ( is       => 'ro',
+                     isa      => 'Str',
+                     lazy     => 1,
+                     default  => sub {
+                         my $self = shift;
+                         return join( "/", $ENV{HOME}, "wubot", "sql", "command" );
+                     },
+                 );
+
+
+
 
 sub react {
     my ( $self, $message, $config ) = @_;
 
     my $output = "";
 
+    my $command;
     if ( $config->{command} ) {
         $self->logger->debug( "Running configured command: $config->{command}" );
-        $output = `$config->{command} 2>&1`;
+        $command = $config->{command};
     }
     elsif ( $config->{command_field} ) {
-        my $command = $message->{ $config->{command_field} };
+        $command = $message->{ $config->{command_field} };
+
+        unless ( $command ) {
+            $self->logger->error( "ERROR: command_field $config->{command_field} is blank, no command executed" );
+            return $message;
+        }
+
         $self->logger->debug( "Running command field: $config->{command_field}: $command " );
+        $output = `$command 2>&1`;
+    }
+    else {
+        $self->logger->error( "Command reactor error: no command or command_field specified in config" );
+        return $message;
+    }
+
+    if ( $config->{fork} ) {
+        return $self->fork_or_enqueue( $command, $message, $config );
+    }
+    else {
         $output = `$command 2>&1`;
     }
 
@@ -40,6 +90,165 @@ sub react {
     }
 
     return $message;
+}
+
+sub monitor {
+    my ( $self ) = @_;
+
+    # clean up any child processes that have exited
+    waitpid(-1, WNOHANG);
+
+    my @messages;
+
+    my $directory = $self->logdir;
+
+    my $dir_h;
+    opendir( $dir_h, $directory ) or die "Can't opendir $directory: $!";
+
+    FILE:
+    while ( defined( my $entry = readdir( $dir_h ) ) ) {
+        next unless $entry;
+        next if -d $entry;
+
+        next unless $entry =~ m|\.log$|;
+        $self->logger->info( "Found entry: $entry" );
+
+        my $id = $entry;
+        $id =~ s|\.log$||;
+
+        next if -r "$id.pid";
+
+        my $logfile = "$directory/$id.log";
+
+        open(my $fh, "<", $logfile)
+            or die "Couldn't open $logfile for reading: $!\n";
+        my $output = "";
+
+        while ( my $line = <$fh> ) {
+            $output .= $line;
+        }
+        close $fh or die "Error closing file: $!\n";
+
+        next FILE unless $output;
+
+        chomp $output;
+
+        my $message;
+        $message->{command_output} = $output;
+
+        push @messages, $message;
+
+        # TODO: hole here where message could be lost after the log is deleted!
+
+        unlink( $logfile );;
+    }
+
+    closedir( $dir_h );
+
+    # start commands
+  QUEUE:
+    while ( my ( $message, $callback ) = $self->queue->get( $self->queuedir ) ) {
+
+        if ( -r $message->{pidfile} ) {
+            $self->logger->debug( "Previous process not yet cleaned up: $message->{pidfile}" );
+            next QUEUE;
+        }
+        if ( -r $message->{logfile} ) {
+            $self->logger->debug( "Previous logfile not yet cleaned up: $message->{logfile}" );
+        }
+
+        if ( my $message = $self->try_fork( $message ) ) {
+
+            # TODO: reacto to message
+
+            # delete the message from the queue
+            $callback->();
+        }
+
+    }
+
+    return \@messages;
+}
+
+sub fork_or_enqueue {
+    my ( $self, $command, $message, $config ) = @_;
+
+    my $id = $config->{fork};
+
+    unless ( -d $self->logdir ) {
+        mkpath( $self->logdir );
+    }
+
+    my $logfile = join( "/", $self->logdir, "$id.log" );
+    my $pidfile = join( "/", $self->logdir, "$id.pid" );
+
+    if ( -r $pidfile ) {
+
+        my $queue = { logfile    => $logfile,
+                      pidfile    => $pidfile,
+                      command    => $command,
+                      id         => $id,
+                      lastupdate => time,
+                  };
+        $self->queue->store( $queue, $self->queuedir );
+
+        $message->{command_queued} = 1;
+        return $message;
+    }
+
+    return $self->try_fork( { id      => $id,
+                              message => $message,
+                              command => $command,
+                              logfile => $logfile,
+                              pidfile => $pidfile,
+                          } );
+}
+
+sub try_fork {
+    my ( $self, $process ) = @_;
+
+    $self->logger->debug( "TRYING FORK: ", YAML::Dump $process );
+
+    my $message = $process->{message} || {};
+
+    if ( my $pid = fork() ) {
+        open(my $fh, ">", $process->{pidfile})
+            or die "Couldn't open $process->{pidfile} for writing: $!\n";
+        print $fh $pid;
+        close $fh or die "Error closing file: $!\n";
+
+        $message->{pidfile}     = $process->{pidfile};
+        $message->{logfile}     = $process->{logfile};
+        $message->{command_pid} = $pid;
+        $message->{id}          = $process->{id};
+        return $message;
+    }
+
+    $self->logger->info( "Pidfile: $process->{pidfile}" );
+
+    # wu - ugly bug fix - when closing STDIN, it becomes free and
+    # may later get reused when calling open (resulting in error
+    # 'Filehandle STDIN reopened as $fh only for output'). :/ So
+    # instead of closing, just re-open to /dev/null.
+    open STDIN, '<', '/dev/null'       or die "$!";
+
+    if ( -r $process->{logfile} ) {
+        unlink $process->{logfile};
+    }
+
+    open STDOUT, '>>', $process->{logfile} or die "Can't write stdout to $process->{logfile}: $!";
+    open STDERR, '>>', $process->{logfile} or die "Can't write stderr to $process->{logfile}: $!";
+
+    setsid or die "Can't start a new session: $!";
+
+    system( $process->{command} );
+
+    unlink( $process->{pidfile} );
+
+    close STDOUT;
+    close STDERR;
+
+    exit;
 }
 
 1;
--- t/react_command.t
+++ t/react_command.t
@@ -2,34 +2,200 @@
 use strict;
 use warnings;
 
+use Test::More tests => 23;
+
+use File::Temp qw/ tempdir /;
 use Log::Log4perl qw(:easy);
-use Test::More 'no_plan';
 use YAML;
 
 use Wubot::Logger;
 use Wubot::Reactor::Command;
 
-ok( my $command = Wubot::Reactor::Command->new(),
-    "Creating new console reactor object"
-);
+my $tempdir  = tempdir( "/tmp/tmpdir-XXXXXXXXXX", CLEANUP => 1 );
+my $queuedir = tempdir( "/tmp/tmpdir-XXXXXXXXXX", CLEANUP => 1 );
 
-my $pwd = `pwd`;
-chomp $pwd;
+{
+    ok( my $command = Wubot::Reactor::Command->new( { logdir => $tempdir } ),
+        "Creating new command reactor object"
+    );
 
-is( $command->react( { }, { command => 'pwd' } )->{command_output},
-    $pwd,
-    "Checking react() run with a configured command"
-);
+    my $pwd = `pwd`;
+    chomp $pwd;
 
-is( $command->react( { test => 'pwd' }, { command_field => 'test' } )->{command_output},
-    $pwd,
-    "Checking react() run with a command from a field"
-);
+    is_deeply( $command->react( { }, { command => 'pwd' } ),
+               { command_output => $pwd },
+               "Checking react() run with a configured command"
+           );
 
-is( $command->react( { test => 'pwd' },
-                     { command_field => 'test', output_field => 'test_output' }
-                 )->{test_output},
-    $pwd,
-    "Checking react() with specified output field"
-);
+    is( $command->react( { test => 'pwd' }, { command_field => 'test' } )->{command_output},
+        $pwd,
+        "Checking react() run with a command from a field"
+    );
 
+    is( $command->react( { test => 'pwd' },
+                         { command_field => 'test', output_field => 'test_output' }
+                     )->{test_output},
+        $pwd,
+        "Checking react() with specified output field"
+    );
+}
+
+
+{
+    my $id = 'forker';
+
+    my $command = Wubot::Reactor::Command->new( { logdir => $tempdir } );
+
+    my $results_h = $command->react( { foo => 'abc' }, { command => 'sleep 3 && echo finished', fork => $id } );
+
+    is( $results_h->{foo},
+        'abc',
+        "Checking react() returned original message after forking"
+    );
+
+    is( $results_h->{command_output},
+        undef,
+        "Checking react() run with a forked command"
+    );
+
+    ok( $results_h->{command_pid},
+        "Checking react() returned command_pid of forked process"
+    );
+
+    my $lockfile    = join( "/", $command->logdir, "$id.pid" );
+    my $logfile     = join( "/", $command->logdir, "$id.log" );
+    my $resultsfile = join( "/", $command->logdir, "$id.yaml" );
+
+    sleep 1;
+
+    ok( -r $lockfile,
+        "Checking that lockfile was created: $lockfile"
+    );
+
+    sleep 4;
+
+    ok( ! -r $lockfile,
+        "Checking that lockfile was removed: $lockfile"
+    );
+
+    ok( -r $logfile,
+        "Checking that logfile was created"
+    );
+
+    is_deeply( $command->monitor(),
+               [ { command_output => 'finished' } ],
+               "Checking background command results"
+           );
+
+    ok( ! -r $logfile,
+        "Checking that logfile was removed when monitor() returned results"
+    );
+
+
+}
+
+{
+    my $id = 'separate';
+
+    my $command = Wubot::Reactor::Command->new( { logdir => $tempdir, queuedir => $queuedir } );
+
+    my $results1_h = $command->react( { foo => 'abc' }, { command => 'sleep 1 && echo finished1', fork => "$id.1" } );
+    my $results2_h = $command->react( { foo => 'def' }, { command => 'sleep 1 && echo finished2', fork => "$id.2" } );
+
+    ok( $results1_h->{command_pid},
+        "Checking react() returned command_pid of forked process"
+    );
+
+    ok( ! $results1_h->{command_queued},
+        "Checking react() for first process was not queued"
+    );
+
+    ok( $results2_h->{command_pid},
+        "Checking react() for second process was not forked"
+    );
+
+    ok( ! $results2_h->{command_queued},
+        "Checking react() for second process was queued"
+    );
+
+    sleep 3;
+
+    is_deeply( $command->monitor(),
+               [
+                   { command_output => 'finished1' },
+                   { command_output => 'finished2' },
+               ],
+               "Checking background command results"
+           );
+}
+
+{
+    my $id = 'double';
+
+    my $command = Wubot::Reactor::Command->new( { logdir => $tempdir } );
+
+    my $results1_h = $command->react( { foo => 'abc' }, { command => 'sleep 1 && echo finished1', fork => $id } );
+    my $results2_h = $command->react( { foo => 'def' }, { command => 'sleep 1 && echo finished2', fork => $id } );
+
+    ok( $results1_h->{command_pid},
+        "Checking react() returned command_pid of forked process"
+    );
+
+    ok( ! $results1_h->{command_queued},
+        "Checking react() for first process was not queued"
+    );
+
+    ok( ! $results2_h->{command_pid},
+        "Checking react() for second process was not forked"
+    );
+
+    ok( $results2_h->{command_queued},
+        "Checking react() for second process was queued"
+    );
+
+    sleep 3;
+
+    is_deeply( $command->monitor(),
+               [
+                   { command_output => 'finished1' },
+               ],
+               "Checking first background command results received when monitor() called"
+           );
+
+    sleep 3;
+
+    is_deeply( $command->monitor(),
+               [
+                   { command_output => 'finished2' },
+               ],
+               "Checking second background command results received when monitor() called"
+           );
+}
+
+
+# {
+#     my $id = 'multi';
+
+#     my $command = Wubot::Reactor::Command->new( { logdir => $tempdir } );
+
+#     $command->react( { foo => 'a' }, { command => 'echo 1 >> $log && sleep 1 && echo 2',  fork => $id } );
+#     $command->react( { foo => 'b' }, { command => 'echo 3 && sleep 1 && echo 4',  fork => $id } );
+#     $command->react( { foo => 'c' }, { command => 'echo 5 && sleep 1 && echo 6',  fork => $id } );
+#     $command->react( { foo => 'd' }, { command => 'echo 7 && sleep 1 && echo 8',  fork => $id } );
+#     $command->react( { foo => 'e' }, { command => 'echo 9 && sleep 1 && echo 10', fork => $id } );
+
+#     sleep 5;
+
+#     my $logfile    = join( "/", $command->logdir, "$id.log" );
+
+
+# }
+
+
+# TODO: get exit status for forked and non-forked commands
+# TODO: get results after command completes - exit status
+# TODO: fork when pid file exists but pid is not active
+# TODO: get results when pid file exists but pid is not active
+# TODO: test background command failure
+# TODO: parent process exits and child keeps running and is picked up by next process
+# TODO: waitpid

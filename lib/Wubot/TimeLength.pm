cacf4429f7910c3e8c2159544838fe3d9c3a2355
Alex White <VVu@geekfarm.org>
2011-09-05 15:48:09 -0700

    bug fix - converted method to be private

--- lib/Wubot/TimeLength.pm
+++ lib/Wubot/TimeLength.pm
@@ -233,7 +233,7 @@ sub get_age_color {
 
     # months
     my $c = $self->_range_map( $seconds, $constants->{M}, $constants->{y}, 250,  0 );
-    return $self->get_hex_color( $c, $c, 0 );
+    return $self->_get_hex_color( $c, $c, 0 );
 }
 
 sub _get_hex_color {
